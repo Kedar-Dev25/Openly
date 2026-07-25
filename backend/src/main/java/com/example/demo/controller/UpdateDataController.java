6c11039b533f8d24b3ab7da55aa266eb44f1d8ed
Kedar-Dev25 <kedar.code7@gmail.com>
2026-07-25 10:06:27 +0530

    feat(owner): implement complete owner status update workflow

---
+++ backend/src/main/java/com/example/demo/controller/UpdateDataController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Shop;
+import com.example.demo.repository.ShopRepository;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+public class UpdateDataController {
+
+    @Autowired
+    ShopRepository repo;
+
+    @PutMapping("/data/update/{id}")
+    public Shop updateStatus(@PathVariable Long id,@RequestBody Shop shop){
+        Shop existingShop = repo.findById(id).orElseThrow();
+        existingShop.setStatus(shop.getStatus());
+        repo.save(existingShop);
+        return existingShop;
+    }
+}
--- frontend/src/Pages/Owner.jsx
+++ frontend/src/Pages/Owner.jsx
@@ -13,18 +13,38 @@ function Owner() {
         .then((response) => {
             console.log("Response:", response.data);
             setShop(response.data);
+            setStatus(response.data.status)
         })
         .catch((error) => {
             console.log(error);
         });
     }, [id]);
+    if (!shop) {
+    return <h2>Loading shop...</h2>;
+    }
     return (
         <>
             <h1>{shop?.name}</h1>
             <h5>{shop?.address}</h5>
-            <h4>{shop?.status}</h4>
-            <button onClick={() =>{setStatus("open")}}>Open</button>
-            <button onClick={() =>{setStatus("close")}}>Close</button>
+            <h4>{status}</h4>
+            <button onClick={() =>{
+                if(status === "OPEN"){
+                    console.log("already open !!!!!!!")
+                    return;
+                }
+                setStatus("OPEN")
+                axios.put(`http://localhost:8080/data/update/${id}`,{status : "OPEN"})
+                }
+            }>Open</button>
+            <button onClick={() =>{
+                if(status === "CLOSE"){
+                    console.log("already closed !!!!!!!")
+                    return;
+                }
+                setStatus("CLOSE")
+                axios.put(`http://localhost:8080/data/update/${id}`,{status : "CLOSE"})
+                }
+            }>Close</button>
         </>
     );
 }
