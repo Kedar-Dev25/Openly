7cf680228fff791de0179550f67862a9d36b1c47
Kedar-Dev25 <kedar.code7@gmail.com>
2026-07-26 20:10:02 +0530

    Remove .env from version control and ignore environment files

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,14 +4,43 @@ import "../App.css";
 
 function Home() {
     const [shop, setShop] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get("http://localhost:8080/all-data")
             .then((response) => {
                 setShop(response.data);
             })
+            .finally(() => {
+            setLoading(false);
+            });
     }, []);
+    if (loading) {
+    return (
+        <>
+            <div className="hero">
+                <h1>LocalKart</h1>
 
+                <p>
+                    Save time. Check if the shop is open
+                    <span> from home.</span>
+                </p>
+            </div>
+
+            <div className="shop-container">
+
+                {[1,2,3].map((item) => (
+                    <div className="card" key={item}>
+                        <div className="skeleton title"></div>
+                        <div className="skeleton address"></div>
+                        <div className="skeleton status"></div>
+                    </div>
+                ))}
+
+            </div>
+        </>
+    );
+}
     return (
         <>
     <div className="hero">
